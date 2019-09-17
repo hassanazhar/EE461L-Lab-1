925e46d2274511de94880234331eb9dabc97d7a5
ryank762 <ryank762@utexas.edu>
2019-09-17 03:05:46 -0500

    finished displaying weather information in text

--- app/src/main/java/com/example/lab1/LocationActivity.java
+++ app/src/main/java/com/example/lab1/LocationActivity.java
@@ -3,12 +3,119 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
 
 public class LocationActivity extends AppCompatActivity {
+    private static final String DARK_SKY_API_KEY = "abe7f3a9a6f05474acd945b2577bd8c3";
+    private static final String LOCATION_LATITUDE = "location_latitude";
+    private static final String LOCATION_LONGTITUDE = "location_longitude";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        Double latitude = getIntent().getDoubleExtra(LOCATION_LATITUDE, 0);
+        Double longitude = getIntent().getDoubleExtra(LOCATION_LONGTITUDE, 0);
+
+        String weatherUrl = "https://api.darksky.net/forecast/" + DARK_SKY_API_KEY + "/" +
+                latitude + "," + longitude;
+
+        System.out.println(weatherUrl);
+
+        displayMap(latitude, longitude);
+        getWeatherInformation(weatherUrl);
+    }
+
+    public void displayMap(Double latitude, Double longitude) {
+        // TODO:
+    }
+
+    public void displayWeatherInformation(Double temperature, Double humidity, Double windSpeed, Double precipitation) {
+        TextView temperatureView = (TextView) findViewById(R.id.temperature_text);
+        String temperatureText = String.format(Locale.US, "Temperature: %.2f\u2109", temperature);
+        temperatureView.setText(temperatureText);
+
+        TextView humidityView = (TextView) findViewById(R.id.humidity_text);
+        String humidityText = String.format(Locale.US, "Humidity: %.0f%%", humidity * 100);
+        humidityView.setText(humidityText);
+
+        TextView windSpeedView = (TextView) findViewById(R.id.wind_speed_text);
+        String windSpeedText = String.format(Locale.US, "Wind Speed: %.2f mph", windSpeed);
+        windSpeedView.setText(windSpeedText);
+
+        TextView precipitationView = (TextView) findViewById(R.id.precipitation_text);
+        String precipitationText = String.format(Locale.US, "Precipitation: %.0f%%", precipitation * 100);
+        precipitationView.setText(precipitationText);
+    }
+
+    /*
+     The structure for this method is outlined in https://developer.android.com/training/volley.
+     */
+    public void getWeatherInformation(String url) {
+        RequestQueue requestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("Response: " + response);
+
+                try {
+                    JSONObject currently = response.getJSONObject("currently");
+                    System.out.println("Currently:" + currently);
+
+                    Double temperature = currently.getDouble("temperature");
+                    Double humidity = currently.getDouble("humidity");
+                    Double windSpeed = currently.getDouble("windSpeed");
+                    Double precipitation = currently.getDouble("precipProbability");
+
+                    System.out.println("Temperature: " + temperature);
+                    System.out.println("Humidity: " + humidity);
+                    System.out.println("Wind speed: " + windSpeed);
+                    System.out.println("Precipitation: " + precipitation);
+
+                    displayWeatherInformation(temperature, humidity, windSpeed, precipitation);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                System.out.println(error);
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import android.widget.TextView;
-
 import com.android.volley.Cache;
 import com.android.volley.Network;
 import com.android.volley.Request;
@@ -19,12 +17,6 @@ import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.maps.*;
-import com.google.maps.errors.ApiException;
-import com.google.maps.model.GeocodingResult;
-import com.google.maps.model.LatLng;
-import java.io.IOException;
-import java.net.URL;
 //import com.android.volley;
 
 import org.json.JSONArray;
@@ -32,6 +24,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String GOOGLE_MAPS_API_KEY = " AIzaSyCVrdaCwAUVATrLXC9xUZaOqIir1v1B39o";
+    private static final String LOCATION_LATITUDE = "location_latitude";
+    private static final String LOCATION_LONGTITUDE = "location_longitude";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,34 +35,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void locationOnClick (View view) {
-
-
         // Get the text view.
         EditText showLocationEditText = (EditText) findViewById(R.id.locationInputText);
 
         // Get the value of the text view.
         String locationString = showLocationEditText.getText().toString();
         locationString = locationString.replace(' ','+');
-        String url= "https://maps.googleapis.com/maps/api/geocode/json?address="+locationString+"&key=AIzaSyCVrdaCwAUVATrLXC9xUZaOqIir1v1B39o";
+        String locationUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                locationString + "&key=" + GOOGLE_MAPS_API_KEY;
 
+        System.out.println(locationUrl);
+        geocodeLocation(locationUrl);
+    }
+
+    public void startLocationActivity(Double latitude, Double longtitude) {
+        Intent locationIntent = new Intent(this, LocationActivity.class);
+
+        locationIntent.putExtra(LOCATION_LATITUDE, latitude);
+        locationIntent.putExtra(LOCATION_LONGTITUDE, longtitude);
+
+        startActivity(locationIntent);
+    }
+
+    /*
+     The structure for this method is outlined in https://developer.android.com/training/volley.
+     */
+    public void geocodeLocation(String url) {
         RequestQueue requestQueue;
 
-// Instantiate the cache
+        // Instantiate the cache
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
 
-// Set up the network to use HttpURLConnection as the HTTP client.
+        // Set up the network to use HttpURLConnection as the HTTP client.
         Network network = new BasicNetwork(new HurlStack());
 
-// Instantiate the RequestQueue with the cache and network.
+        // Instantiate the RequestQueue with the cache and network.
         requestQueue = new RequestQueue(cache, network);
 
-// Start the queue
+        // Start the queue
         requestQueue.start();
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, new Response.Listener<JSONObject>(){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, new Response.Listener<JSONObject>() {
             @Override
-
-            public void onResponse(JSONObject response){
+            public void onResponse(JSONObject response) {
                 System.out.println("Response: " + response);
+
                 try {
                     JSONArray results = response.getJSONArray("results");
                     System.out.println("Results: " + results);
@@ -83,20 +94,19 @@ public class MainActivity extends AppCompatActivity {
 
                     System.out.println("Lat: " + lat);
                     System.out.println("Lng: " + lng);
+                    startLocationActivity(lat, lng);
+
                 } catch (JSONException e) {
-                    System.out.println("asdf");
+                    e.printStackTrace();
                 }
             }
-        },
-                new Response.ErrorListener(){
-            @Override
+        }, new Response.ErrorListener(){
+                    @Override
                     public void onErrorResponse(VolleyError error){
                         System.out.println(error);
-            }
+                    }
                 });
+
         requestQueue.add(jsonObjectRequest);
-
-
-
     }
 }
